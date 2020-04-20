ffebfcfdde292ad15aadbb3b96e130f797597b84
Renan Quintino <renanquintino15@gmail.com>
2020-04-20 16:37:50 -0300

    Iniciação do progrma

---
+++ testandogithub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testandogithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
